88d8fffeffb8add093a36c9b808f43927dc80fdc
Daniel Ortiz <thisisdanielortiz@gmail.com>
2023-03-31 14:35:34 -0600

    bubble sort algorithm

---
+++ bubble_sort.py
@@ -0,0 +1,14 @@
+def buuble_sort(my_list):
+    for i in range(len(my_list)-1, 0, -1):
+        for j in range(i):
+            if my_list[j] > my_list[j+1]:
+                temp = my_list[j]
+                my_list[j] = my_list[j+1]
+                my_list[j+1] = temp
+
+
+    
+    return my_list
+
+
+print(buuble_sort([5,2,7,1,0,1,4,8,10]))
